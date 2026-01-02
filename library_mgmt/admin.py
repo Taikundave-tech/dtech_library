7e27d7a43bce8bfad842777942e9d0a4025baf0c
Dave -tech <dsha94633@gmail.com>
2026-01-02 19:38:15 +0300

    Update admin.py

--- library_mgmt/admin.py
+++ library_mgmt/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import User,Resource,LibrarySession
+from .models import Book  # Import your Book model
 
+# This line tells Django to show "Books" in the admin panel
+admin.site.register(Book)
 admin.site.register(User)
 admin.site.register(Resource)
 admin.site.register(LibrarySession)
