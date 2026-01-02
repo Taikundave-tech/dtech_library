e35442b16ddcd099d11f8368e7781bf1be57c2a1
Dave -tech <dsha94633@gmail.com>
2026-01-02 20:31:31 +0300

    Update settings.py

--- dtech_library/settings.py
+++ dtech_library/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 DATABASES={
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+       default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
